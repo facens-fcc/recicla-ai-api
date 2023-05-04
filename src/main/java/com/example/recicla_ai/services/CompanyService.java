26bcd831a569b3d178830edff015061d8219fc1a
Matheus Carvalho <henriquematheus.c@gmail.com>
2023-05-03 22:20:40 -0300

    feat: add CompanyService

--- src/main/java/com/example/recicla_ai/services/CompanyService.java
+++ src/main/java/com/example/recicla_ai/services/CompanyService.java
@@ -1,5 +1,9 @@
 package com.example.recicla_ai.services;
 
+import com.example.recicla_ai.models.Company;
+
 public interface CompanyService {
-    
+  Company create(Company company);
+  void delete(Long id);
+  void update(Long id, Company company);    
 }
