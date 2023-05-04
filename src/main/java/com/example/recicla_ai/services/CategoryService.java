2effaf964b46ce8fe28ba1cd178b5719c07fb3eb
Matheus Carvalho <henriquematheus.c@gmail.com>
2023-05-03 22:20:21 -0300

    feat: add CategoryService

--- src/main/java/com/example/recicla_ai/services/CategoryService.java
+++ src/main/java/com/example/recicla_ai/services/CategoryService.java
@@ -1,5 +1,10 @@
 package com.example.recicla_ai.services;
 
+import com.example.recicla_ai.dtos.CategoryDTO;
+import com.example.recicla_ai.models.Category;
+
 public interface CategoryService {
-    
+    Category create(CategoryDTO categoryDTO);
+    // void delete(Long id);
+    // void update(Long id, CategoryDTO categoryDTO);
 }
