63c803e78715a60dbf0452d408a059a1e4f098f8
Matheus Carvalho <henriquematheus.c@gmail.com>
2023-05-03 22:16:53 -0300

    feat: add CategoryController

---
+++ src/main/java/com/example/recicla_ai/controllers/CategoryController.java
@@ -0,0 +1,28 @@
+package com.example.recicla_ai.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.recicla_ai.dtos.CategoryDTO;
+import com.example.recicla_ai.models.Category;
+import com.example.recicla_ai.services.CategoryService;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+  private CategoryService categoryService;
+  
+  public CategoryController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Category create(@RequestBody CategoryDTO categoryDTO) {
+    return this.categoryService.create(categoryDTO);
+  }
+}
