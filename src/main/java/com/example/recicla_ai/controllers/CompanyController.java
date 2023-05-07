6207921fe75da648ce71968610f4a3d7384b6d8f
Ingrid Camargo <ingrid.camargopr@gmail.com>
2023-05-07 18:03:26 -0300

    feat: add create method to CompanyController

--- src/main/java/com/example/recicla_ai/controllers/CompanyController.java
+++ src/main/java/com/example/recicla_ai/controllers/CompanyController.java
@@ -1,5 +1,28 @@
 package com.example.recicla_ai.controllers;
 
-public class CompanyController {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.recicla_ai.dtos.CompanyDTO;
+import com.example.recicla_ai.models.Company;
+import com.example.recicla_ai.services.CompanyService;
+
+@RestController
+@RequestMapping("/api/company")
+public class CompanyController {    
+    private CompanyService companyService;
   
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Company create(@RequestBody CompanyDTO companyDTO) {
+        return this.companyService.create(companyDTO);
+    }
 }
