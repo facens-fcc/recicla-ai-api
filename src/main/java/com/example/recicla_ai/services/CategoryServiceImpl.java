bd9e7356a7f59d1be0e38d5709a140997abe73be
Ingrid Camargo <ingrid.camargopr@gmail.com>
2023-05-07 17:54:49 -0300

    refactor: add exception to the create method in the CategoryServiceImpl

--- src/main/java/com/example/recicla_ai/services/CategoryServiceImpl.java
+++ src/main/java/com/example/recicla_ai/services/CategoryServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.recicla_ai.services;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
 import com.example.recicla_ai.dtos.CategoryDTO;
+import com.example.recicla_ai.exceptions.BusinessRuleException;
 import com.example.recicla_ai.models.Category;
+import com.example.recicla_ai.models.Company;
 import com.example.recicla_ai.repositories.CategoryRepository;
 import com.example.recicla_ai.repositories.CompanyRepository;
 
@@ -14,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
   private final CategoryRepository categoryRepository;
+  private final CompanyRepository companyRepository;
 
   @Override
   @Transactional
@@ -21,7 +26,19 @@ public class CategoryServiceImpl implements CategoryService {
     Category category = new Category();
     category.setName(categoryDTO.getName());
     category.setIcon(categoryDTO.getIcon());
-    return categoryRepository.save(category);
-  }
+    category.setCompanies(new ArrayList<>());
 
+    Category savedCategory = categoryRepository.save(category);
+
+    if (categoryDTO.getCompanyIds() != null && !categoryDTO.getCompanyIds().isEmpty()) {
+      for (Long companyId : categoryDTO.getCompanyIds()) {
+        Company company = companyRepository.findById(companyId)
+            .orElseThrow(() -> new BusinessRuleException("Company not found with ID: " + companyId));
+        company.getCategories().add(savedCategory);
+        companyRepository.save(company);
+      }
+    }
+
+    return savedCategory;
+  }
 }
