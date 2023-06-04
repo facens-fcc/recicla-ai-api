b3368ad951671c7acdefe9c51b3d82aeaa336835
Ingrid Camargo <ingrid.camargopr@gmail.com>
2023-06-04 17:15:11 -0300

    refactor: update service methods

--- src/main/java/com/example/recicla_ai/services/CompanyService.java
+++ src/main/java/com/example/recicla_ai/services/CompanyService.java
@@ -1,15 +1,18 @@
 package com.example.recicla_ai.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.recicla_ai.models.Company;
 import com.example.recicla_ai.dtos.CompanyDTO;
-import com.example.recicla_ai.dtos.SearchDataDTO;
+import com.example.recicla_ai.dtos.CompanyDataDTO;
 
 public interface CompanyService {
   Company create(CompanyDTO companyDTO);
   void delete(Long id);
   void update(Long id, CompanyDTO companyDTO);    
-  List<Company> getAll();
+  List<CompanyDataDTO> getAll();
+  // List<Company> getAll();
+  Optional<Company> getById(Long id);
   // public List<CompanyDTO> searchCompanies(SearchDataDTO searchData);
 }
