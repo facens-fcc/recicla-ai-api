b7cd1a54c25ad2341f40dc56a6e4a482654cba57
Ingrid Camargo <ingrid.camargopr@gmail.com>
2023-06-04 20:44:13 -0300

    feat: implement findAllById method to CategoryRepository

--- src/main/java/com/example/recicla_ai/repositories/CategoryRepository.java
+++ src/main/java/com/example/recicla_ai/repositories/CategoryRepository.java
@@ -1,9 +1,21 @@
 package com.example.recicla_ai.repositories;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.recicla_ai.models.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    
+    @Override
+    default List<Category> findAllById(Iterable<Long> ids) {
+        List<Category> categories = new ArrayList<>();
+        for (Long id : ids) {
+            Optional<Category> category = findById(id);
+            category.ifPresent(categories::add);
+        }
+        return categories;
+    }
 }
