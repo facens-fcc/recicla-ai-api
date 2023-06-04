954852febbed6012dd5451bad22d1bd3703764ab
Ingrid Camargo <ingrid.camargopr@gmail.com>
2023-06-04 17:13:15 -0300

    feat: add CompanyDataDTO

---
+++ src/main/java/com/example/recicla_ai/dtos/CompanyDataDTO.java
@@ -0,0 +1,34 @@
+package com.example.recicla_ai.dtos;
+
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class CompanyDataDTO {
+    private Long id;
+    private String name;
+    private boolean whatsapp;
+    private String phoneDdi;
+    private String phoneDdd;
+    private String phoneNumber;
+    private String addressStreet;
+    private String addressNumber;
+    private String addressNeighborhood;
+    private String city;
+    private String state;
+    private String zipCode;
+    private double lat;
+    private double lng;
+    private boolean payment;
+    private boolean residentialCollection;
+    private Set<CategoryDTO> categories;
+    private double distance;
+}
